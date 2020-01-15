ee99ebe79189366f3bd21910ab21d39051c04f3f
aastha.92 <npathak@gmail.com>
2020-01-14 19:34:07 -0600

    updated index.js file

--- sql/connection.js
+++ sql/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+require("dotenv").config()
 
 class Connection {
   constructor() {
@@ -7,9 +8,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.70.5.64',
         user: 'root',
-        password: 'password',
+        password: process.env.MYSQL_PASSWORD,
         database: 'admin'
       }
 
